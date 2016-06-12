4ff70679171a8e1fc749acbfcdd1a449c34de42a
Gant Laborde <gantman@gmail.com>
2016-06-12 12:42:07 -0500

    changed order and added gradle cache buster

--- bin/newclear.sh
+++ bin/newclear.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # Clean everything possible
-rm -rf $TMPDIR/react-*
 watchman watch-del-all
 rm -rf ios/build/ModuleCache/*
+rm -rf $HOME/.gradle/caches/
 rm -rf node_modules/
 npm cache clean
 npm i
+rm -rf $TMPDIR/react-*
