a2708c49ba5b9ac8ff8af907a1565d42efd372f4
Gant Laborde <gantman@gmail.com>
2016-07-07 10:23:21 -0500

    added android clean handles #3

--- bin/newclear.sh
+++ bin/newclear.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 # Clean everything possible
+
+# watchman
 watchman watch-del-all
+
+# ios
 rm -rf ios/build/ModuleCache/*
+
+# android
+rm -rf android/app/build
 rm -rf $HOME/.gradle/caches/
+
+# javascript
 rm -rf node_modules/
 npm cache clean
+
+# Fresh install
 npm i
+
+# clear react temp files (keep this last clean step)
 rm -rf $TMPDIR/react-*
